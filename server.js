95e7ab5b864e13911998208daa7af7b0e243f3e7
smrsun4 <smrsun4@gmail.com>
2024-08-06 12:24:26 -0400

    changed names for my routes folder

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
-const front = require('./routes/front.js');
-const api = require('./routes/api.js');
-
+const notes = require('./routes/notes.js');
+const index = require('./routes/index.js');
+const path = require('path');
 const PORT = 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.use('/', front)
+app.use('/', notes);
 
-app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT}`)
-  );
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
