de3b337d266d6dde93702a1b982091539158af52
smrsun4 <smrsun4@gmail.com>
2024-08-06 22:30:00 -0400

    added returns for addNote and deleteNote

--- db/store.js
+++ db/store.js
@@ -1,5 +1,9 @@
+const util = require('util')
+const fs = require('fs')
 const readFromFile = util.promisify(fs.readFile)
-const writeToFile = util.promisify(fs.writeToFile)
+const writeToFile = util.promisify(fs.writeFile)
+const uuid = require('uuid')
+
 class Store {
     read() {
         return readFromFile('db/db.json','utf8')
@@ -9,18 +13,31 @@ class Store {
     }
 
     getNotes() {
+        console.log("hi")
         return this.read()
+       
     }
 
     addNote(note) {
         const{ title, text } = note
+        console.log(title)
         if (!title||!text) {
             throw new Error(`Please add a title or text to your note.`);
-
         }
+        const newNote = {title, text, id:uuid()}
+        console.log("new note:", newNote); 
+        return this.getNotes()
+        .then((notes) => [...notes, newNote])
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => newNote)
     }
 
-    deleteNote() {
-
+    deleteNote(id) {
+        return this.getNotes()
+        .then((notes) => notes.filter((note) => note.id !== id))
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => id);
     }
 }
+
+module.exports = new Store()
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
-const db = require('../db/db.json');
-
+// const db = require('../db/db.json');
+const store = require('../db/store');
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
-router.get('/', (req, res) => {
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  console.log(`Returned saved notes.`);
+router.get('/notes', (req, res) => {
+  store.getNotes().then((notes) => {
+    return res.json(notes);
+  });
+
 });
+
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+router.post('')
+
 
 module.exports = router;
