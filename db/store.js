bfba0ac1a15c1f92bec707def26fc8509a91d6a3
smrsun4 <smrsun4@gmail.com>
2024-08-06 22:56:13 -0400

    added router.post to the index.js

--- db/store.js
+++ db/store.js
@@ -1,43 +1,42 @@
-const util = require('util')
-const fs = require('fs')
-const readFromFile = util.promisify(fs.readFile)
-const writeToFile = util.promisify(fs.writeFile)
-const uuid = require('uuid')
+const util = require('util');
+const fs = require('fs');
+const readFromFile = util.promisify(fs.readFile);
+const writeToFile = util.promisify(fs.writeFile);
+const uuid = require('uuid');
 
 class Store {
-    read() {
-        return readFromFile('db/db.json','utf8')
-    }
-    write(note) {
-        return writeToFile('db/db.json', JSON.stringify(note))
-    }
+  read() {
+    return readFromFile('db/db.json', 'utf8');
+  }
+  write(note) {
+    return writeToFile('db/db.json', JSON.stringify(note));
+  }
 
-    getNotes() {
-        console.log("hi")
-        return this.read()
-       
-    }
+  getNotes() {
+    console.log('hi');
+    return this.read();
+  }
 
-    addNote(note) {
-        const{ title, text } = note
-        console.log(title)
-        if (!title||!text) {
-            throw new Error(`Please add a title or text to your note.`);
-        }
-        const newNote = {title, text, id:uuid()}
-        console.log("new note:", newNote); 
-        return this.getNotes()
-        .then((notes) => [...notes, newNote])
-        .then((updatedNotes) => this.write(updatedNotes))
-        .then(() => newNote)
+  addNote(note) {
+    const { title, text } = note;
+    console.log(title);
+    if (!title || !text) {
+      throw new Error(`Please add a title or text to your note.`);
     }
+    const newNote = { title, text, id: uuid() };
+    console.log('new note:', newNote);
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
 
-    deleteNote(id) {
-        return this.getNotes()
-        .then((notes) => notes.filter((note) => note.id !== id))
-        .then((updatedNotes) => this.write(updatedNotes))
-        .then(() => id);
-    }
+  deleteNote(id) {
+    return this.getNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => id);
+  }
 }
 
-module.exports = new Store()
+module.exports = new Store();
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,24 @@
 const router = require('express').Router();
 // const db = require('../db/db.json');
 const store = require('../db/store');
+const uuid = require('uuid');
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 router.get('/notes', (req, res) => {
   store.getNotes().then((notes) => {
     return res.json(notes);
   });
-
 });
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-router.post('')
-
+router.post('/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuid();
+  store
+    .addNote(newNote)
+    .then(() => {})
+    .catch((err) => {
+      res.status(500).json({ error: 'Failed to add note.' });
+    });
+});
 
 module.exports = router;
